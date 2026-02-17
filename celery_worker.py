77d845ba34639db8a345cd44de9ef238e44fc78f
ShyamSundaraChary <shyamsundarachary22@gmail.com>
2026-02-17 16:54:23 +0530

    Updated the configuraiotn of celery

--- celery_worker.py
+++ celery_worker.py
@@ -1,28 +1,20 @@
 from celery import Celery
 
-# Celery configuration
-# In a production environment, you would typically use environment variables for these settings.
-# For this example, we're using Redis running locally on the default port.
-# The broker is responsible for receiving and sending messages between the main application and the worker.
-# The backend is where Celery stores the results of tasks. In this case, we're using Redis for both.
-# Make sure to have Redis installed and running on your machine for this to work.
 
 celery = Celery( 
     'report_tasks', # Name of the Celery app
-    broker='redis://localhost:6379/0', # Redis broker URL
-    backend='redis://localhost:6379/0' # Redis backend URL
+    broker='redis://localhost:6379/0', # Redis broker URL, this is where Celery will look for tasks to execute
+    backend='redis://localhost:6379/0' # Redis backend URL, this is where Celery will store the results of tasks
 )
 
 # Optional Celery configuration settings
 celery.conf.update(
     task_track_started=True,# Track when a task starts
-    result_expires=3600, # Results expire after 1 hour
-    worker_prefetch_multiplier=1, # Prevents one worker from grabbing too many tasks.
-    worker_concurrency = 10 # Number of worker processes to run (adjust based on your machine's capabilities),
-
+    result_expires=3600, # Results expire after 1 hour in the backend, so store results in database for 1 hour before they are deleted.
+    worker_prefetch_multiplier=1 # Prevents one worker from grabbing too many tasks.
+    # It ensures that each worker only fetches one task at a time, which can help improve performance and reduce latency when processing tasks.
 )
 
-# Import tasks to ensure they are registered with Celery
 # This import is necessary to ensure that the tasks defined in tasks.py are registered with the Celery app.
 # without this import, Celery would not be aware of the tasks and would not be able to execute them when they are called from the Flask app.
 import tasks
--- start_worker.sh
+++ start_worker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 source venv/bin/activate
-celery -A celery_worker.celery worker --loglevel=info
+celery -A celery_worker.celery worker --loglevel=info --concurrency=10
+
+# For auto-scaling workers, you can use the following command instead:
+# celery -A celery_worker.celery worker --autoscale=10,2 --loglevel=info
+# here maximum number of workers is 10 and minimum is 2. 
